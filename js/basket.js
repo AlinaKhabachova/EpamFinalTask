cd45f3f654efef9dfbbe080605492467c05b26aa
Alina Khabachova <alina.khabachyova@gmail.com>
2016-02-13 17:34:35 +0200

    Fix gallery

--- js/basket.js
+++ js/basket.js
@@ -6,5 +6,34 @@ function showBasket() {
     picture.className = "pictureOfItem";
     var removeItem = document.createElement('div');
     removeItem.className = "deleteItem";
+    var name = document.createElement('h3');
 
+
+//<h3>Floral Plimsoll (1st)</h3>
+//    <p>Ref. 2514/302</p>
+//    <p>Black</p>
+//    <p>39</p>
+//    <form>
+//    <input type="number" name="quantity" min="1" max="99" value="1">
+//        </form>
+//        <p class="sum">&euro;99.95</p>
 }
+
+localStorage['basket'] = [
+        {
+            ref : "2514/302",
+            collor: "black",
+            name: "name",
+            price: 20,
+            size: 32,
+            count: 1
+        },
+        {
+            ref : "2514/302",
+            collor: "white",
+            name: "name 2",
+            price: 30,
+            size: 40,
+            count: 2
+        }
+    ];
--- js/pdp.js
+++ js/pdp.js
@@ -1,7 +1,13 @@
 $(document).ready(function () {
+
     $('.crop-img').click(function () {
         var img = $('#bigImage').attr('src');
         $('#bigImage').attr('src', $(this).attr('src'));
         $(this).attr('src', img);
     });
+
+    $('#addToCard').click(function(){
+        $('')
+    });
 });
+
